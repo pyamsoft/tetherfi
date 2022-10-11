541811bacac48c6ac51312fed2403ef866e4773a
pyamsoft <developer.pyamsoft@gmail.com>
2022-10-11 12:22:40 -0700

    Limit font weights to just 400 and 700

--- app/src/main/java/com/pyamsoft/tetherfi/TetherFiTheme.kt
+++ app/src/main/java/com/pyamsoft/tetherfi/TetherFiTheme.kt
@@ -8,12 +8,14 @@ import androidx.compose.material.Colors
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.google.android.material.R
 import com.pyamsoft.pydroid.theme.PYDroidTheme
@@ -73,6 +75,62 @@ private fun themeShapes(): Shapes {
 }
 
 @Composable
+@CheckResult
+private fun themeTypography(): Typography {
+  val typography = MaterialTheme.typography
+  return typography.copy(
+      h1 =
+          typography.h1.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      h2 =
+          typography.h2.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      h3 =
+          typography.h3.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      h4 =
+          typography.h4.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      h5 =
+          typography.h5.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      h6 =
+          typography.h6.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      subtitle1 =
+          typography.subtitle1.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      subtitle2 =
+          typography.subtitle2.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      body1 =
+          typography.body1.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      body2 =
+          typography.body2.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      caption =
+          typography.caption.copy(
+              fontWeight = FontWeight.W400,
+          ),
+      overline =
+          typography.overline.copy(
+              fontWeight = FontWeight.W400,
+          ),
+  )
+}
+
+@Composable
 fun Activity.TetherFiTheme(
     themeProvider: ThemeProvider,
     content: @Composable () -> Unit,
@@ -98,6 +156,7 @@ fun Activity.TetherFiTheme(
   PYDroidTheme(
       colors = themeColors(this, isDarkMode),
       shapes = themeShapes(),
+      typography = themeTypography(),
   ) {
     // We update the LocalContentColor to match our onBackground. This allows the default
     // content color to be more appropriate to the theme background
