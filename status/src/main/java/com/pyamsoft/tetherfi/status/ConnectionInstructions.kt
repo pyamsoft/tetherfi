5b4df216d3e2136273094cc47512af6d54df5e1b
pyamsoft <developer.pyamsoft@gmail.com>
2022-10-10 23:43:41 -0700

    import

--- status/src/main/java/com/pyamsoft/tetherfi/status/ConnectionInstructions.kt
+++ status/src/main/java/com/pyamsoft/tetherfi/status/ConnectionInstructions.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
