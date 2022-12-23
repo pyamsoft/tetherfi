e5931dd11a3018ec4db363836a5d03b7eae837a3
pyamsoft <developer.pyamsoft@gmail.com>
2022-12-23 14:53:41 -0800

    Prep tile UI

--- tile/src/main/java/com/pyamsoft/tetherfi/tile/ProxyTileScreen.kt
+++ tile/src/main/java/com/pyamsoft/tetherfi/tile/ProxyTileScreen.kt
@@ -1,6 +1,7 @@
 package com.pyamsoft.tetherfi.tile
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,6 +18,7 @@ import com.pyamsoft.pydroid.theme.keylines
 import com.pyamsoft.pydroid.ui.defaults.DialogDefaults
 import com.pyamsoft.tetherfi.server.status.RunningStatus
 import kotlinx.coroutines.delay
+import timber.log.Timber
 
 @Composable
 fun ProxyTileScreen(
@@ -33,24 +35,123 @@ fun ProxyTileScreen(
 
   val isShowing = state.isShowing
 
-  // Status update
-  LaunchedEffect(
-      status,
-      onStatusUpdated,
-  ) {
-    onStatusUpdated(status)
-  }
+  val isMiddleStep =
+      remember(
+          status,
+          initialStatus,
+      ) {
+        when (initialStatus) {
+          is RunningStatus.NotRunning -> {
+            // We are at least middle if we are starting or started
+            return@remember status is RunningStatus.Starting || status is RunningStatus.Running
+          }
+          is RunningStatus.Running -> {
+            // We are at least middle if we are stopping or stopped
+            return@remember status is RunningStatus.Stopping || status is RunningStatus.NotRunning
+          }
+          else -> {
+            Timber.w("Unexpected middle step, we are in progress so mark us as middle: $status")
+            return@remember true
+          }
+        }
+      }
 
-  // Once the dialog is flagged off, we fire this "completed" hook
-  LaunchedEffect(
-      isShowing,
-      onComplete,
+  val isFinalStep =
+      remember(
+          status,
+          initialStatus,
+      ) {
+        when (initialStatus) {
+          is RunningStatus.NotRunning -> {
+            // We are done if we are started
+            return@remember status is RunningStatus.Running
+          }
+          is RunningStatus.Running -> {
+            // We are at done if we are stopped
+            return@remember status is RunningStatus.NotRunning
+          }
+          else -> {
+            Timber.w(
+                "Unexpected final step, we are in progress so do not mark us as final: $status")
+            return@remember false
+          }
+        }
+      }
+
+  SideEffectUpdate(
+      status = status,
+      onStatusUpdated = onStatusUpdated,
+  )
+  SideEffectComplete(
+      isShowing = isShowing,
+      onComplete = onComplete,
+  )
+  SideEffectStep(
+      initialStatus = initialStatus,
+      status = status,
+      onDismissed = onDismissed,
+  )
+
+  Box(
+      modifier = modifier,
+      contentAlignment = Alignment.Center,
   ) {
-    if (!isShowing) {
-      onComplete()
+    if (isShowing) {
+      Dialog(
+          onDismissRequest = onDismissed,
+          properties =
+              DialogProperties(
+                  dismissOnBackPress = isInitialStatusError,
+                  dismissOnClickOutside = isInitialStatusError,
+              ),
+      ) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            elevation = DialogDefaults.Elevation,
+        ) {
+          Column(
+              modifier = Modifier.padding(MaterialTheme.keylines.content),
+          ) {
+            Text(
+                text =
+                    "Status: ${when (status) {
+                          is RunningStatus.Error -> "ERROR"
+                          is RunningStatus.NotRunning -> "STOPPED"
+                          is RunningStatus.Running -> "RUNNING"
+                          is RunningStatus.Starting -> "STARTING..."
+                          is RunningStatus.Stopping -> "STOPPING..."
+                      }
+                      }",
+                style = MaterialTheme.typography.h6,
+            )
+
+            Text(
+                text = "First: YES",
+                style = MaterialTheme.typography.body1,
+            )
+
+            Text(
+                text = "Middle: ${if (isMiddleStep) "YES" else "NO"}",
+                style = MaterialTheme.typography.body1,
+            )
+
+            Text(
+                text = "Final: ${if (isFinalStep) "YES" else "NO"}",
+                style = MaterialTheme.typography.body1,
+            )
+          }
+        }
+      }
     }
   }
+}
 
+@Composable
+private fun SideEffectStep(
+    initialStatus: RunningStatus,
+    status: RunningStatus,
+    onDismissed: () -> Unit,
+) {
   LaunchedEffect(
       initialStatus,
       status,
@@ -87,43 +188,34 @@ fun ProxyTileScreen(
       }
     }
   }
+}
 
-  Box(
-      modifier = modifier,
-      contentAlignment = Alignment.Center,
+@Composable
+private fun SideEffectUpdate(
+    status: RunningStatus,
+    onStatusUpdated: (RunningStatus) -> Unit,
+) {
+  // Status update
+  LaunchedEffect(
+      status,
+      onStatusUpdated,
   ) {
-    if (isShowing) {
-      Dialog(
-          onDismissRequest = onDismissed,
-          properties =
-              DialogProperties(
-                  dismissOnBackPress = isInitialStatusError,
-                  dismissOnClickOutside = isInitialStatusError,
-              ),
-      ) {
-        Surface(
-            shape = MaterialTheme.shapes.medium,
-            elevation = DialogDefaults.Elevation,
-        ) {
-          Box(
-              modifier = Modifier.padding(MaterialTheme.keylines.content),
-              contentAlignment = Alignment.Center,
-          ) {
-            Text(
-                text =
-                    "Status: ${when (status) {
-                          is RunningStatus.Error -> "ERROR"
-                          is RunningStatus.NotRunning -> "STOPPED"
-                          is RunningStatus.Running -> "RUNNING"
-                          is RunningStatus.Starting -> "STARTING..."
-                          is RunningStatus.Stopping -> "STOPPING..."
-                      }
-                      }",
-                style = MaterialTheme.typography.h6,
-            )
-          }
-        }
-      }
+    onStatusUpdated(status)
+  }
+}
+
+@Composable
+private fun SideEffectComplete(
+    isShowing: Boolean,
+    onComplete: () -> Unit,
+) {
+  // Once the dialog is flagged off, we fire this "completed" hook
+  LaunchedEffect(
+      isShowing,
+      onComplete,
+  ) {
+    if (!isShowing) {
+      onComplete()
     }
   }
 }
