e17872a4b3632eb3ed8b54742260e1c4ea9a39f6
pyamsoft <developer.pyamsoft@gmail.com>
2022-12-23 23:56:43 -0800

    Add billing upsell

--- ui/src/main/java/com/pyamsoft/tetherfi/ui/icons/PYDroid.kt
+++ ui/src/main/java/com/pyamsoft/tetherfi/ui/icons/PYDroid.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import com.pyamsoft.pydroid.theme.keylines
+import com.pyamsoft.pydroid.ui.widget.BillingUpsellWidget
 import com.pyamsoft.pydroid.ui.widget.NewVersionWidget
 import com.pyamsoft.pydroid.ui.widget.ShowChangeLogWidget
 import com.pyamsoft.pydroid.ui.widget.UpdateProgressWidget
@@ -31,6 +32,15 @@ fun LazyListScope.renderPYDroidExtras() {
 
   item {
     ShowChangeLogWidget(
+        modifier =
+            Modifier.fillMaxWidth()
+                .padding(horizontal = MaterialTheme.keylines.content)
+                .padding(top = MaterialTheme.keylines.content),
+    )
+  }
+
+  item {
+    BillingUpsellWidget(
         modifier =
             Modifier.fillMaxWidth()
                 .padding(horizontal = MaterialTheme.keylines.content)
