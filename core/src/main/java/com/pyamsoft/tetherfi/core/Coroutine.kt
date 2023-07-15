07450fc3c1ea1e2439f1407f76f269e58e272f59
pyamsoft <developer.pyamsoft@gmail.com>
2023-07-14 19:51:15 -0700

    Update doc, move into pydroid

--- core/src/main/java/com/pyamsoft/tetherfi/core/Coroutine.kt
+++ core/src/main/java/com/pyamsoft/tetherfi/core/Coroutine.kt
@@ -2,25 +2,34 @@ package com.pyamsoft.tetherfi.core
 
 import kotlinx.coroutines.flow.MutableSharedFlow
 
+/**
+ * A SharedFlow once collected by definition will never complete. This effectively causes our
+ * current Coroutine to permanently suspend as it waits forever for a SharedFlow.collect completion
+ * event which will never happen
+ */
 private val neverEndingFlow = MutableSharedFlow<Nothing>()
 
 /**
  * Hold the coroutine "forever" until it is cancelled
  *
- * Useful for start-stop work like such
+ * Useful for start-stop work like such, where start is controlled by launching the Coroutine and
+ * stop is controlled by cancelling i
  *
- * launch { try { coroutineScope { doWorkOnStart()
- *
- *        suspendUntilCancel()
- *      }
- *    } finally {
- *      withContext(NonCancellable) {
- *        doWorkOnCancel()
- *      }
- *    }
- *
+ * ```kotlin
+ * launch {
+ *   try {
+ *     coroutineScope {
+ *       doWorkOnStart()
+ *       suspendUntilCancel()
+ *     }
+ *   } finally {
+ *     withContext(NonCancellable) {
+ *       doWorkOnCancel()
+ *     }
+ *   }
  * }
+ * ```
  */
-suspend fun suspendUntilCancel() {
+public suspend fun suspendUntilCancel(): Nothing {
   neverEndingFlow.collect {}
 }
