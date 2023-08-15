c003fe8e60078bbed9a5dc222f1f741a37f0e63f
pyamsoft <developer.pyamsoft@gmail.com>
2023-08-15 16:39:08 -0700

    31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
   defaultConfig {
     applicationId = "com.pyamsoft.tetherfi"
 
-    versionCode = 30
-    versionName = "20230714-1"
+    versionCode = 31
+    versionName = "20230815-1"
 
     minSdk = rootProject.extra["minSdk"] as Int
     targetSdk = rootProject.extra["targetSdk"] as Int
