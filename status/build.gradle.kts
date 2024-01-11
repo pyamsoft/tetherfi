6c4953f88ba61858284e923b3cfb5182015daabc
pyamsoft <developer.pyamsoft@gmail.com>
2024-01-11 13:56:20 -0800

    Update copyright

--- status/build.gradle.kts
+++ status/build.gradle.kts
@@ -1,9 +1,9 @@
 /*
- * Copyright 2021 Peter Kenji Yamanaka
+ * Copyright 2024 pyamsoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at:
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
